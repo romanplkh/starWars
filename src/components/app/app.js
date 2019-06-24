f27d3b96c82bea211b5cc6535251e7005e0bddc2
Roman Pelikh <pelikh.roman@gmail.com>
2019-06-24 17:30:46 -0300

    hoc

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,74 +4,74 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
 
-import Row from "../row/row";
-import ItemDetails, { Record } from "../item-details/item-details";
-import SwapiService from "../../services/swapi-service";
+import Row from '../row/row';
+import ItemDetails, { Record } from '../item-details/item-details';
+import SwapiService from '../../services/swapi-service';
+
+import ItemList from '../item-list';
 
 import './app.css';
 
 export default class App extends Component {
+	swapiService = new SwapiService();
 
-  swapiService = new SwapiService();
+	state = {
+		showRandomPlanet: true
+	};
 
-  state = {
-    showRandomPlanet: true
-  };
+	toggleRandomPlanet = () => {
+		this.setState(state => {
+			return {
+				showRandomPlanet: !state.showRandomPlanet
+			};
+		});
+	};
 
-  toggleRandomPlanet = () => {
-    this.setState((state) => {
-      return {
-        showRandomPlanet: !state.showRandomPlanet
-      }
-    });
-  };
+	render() {
+		const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
 
-  render() {
+		const {
+			getPerson,
+			getStarship,
+			getPersonImage,
+			getStarshipImage,
+			getAllPeople,
+			getAllPlanets
+		} = this.swapiService;
 
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
+		const personDetails = (
+			<ItemDetails itemId={11} getData={getPerson} getImageUrl={getPersonImage}>
+				<Record field="gender" label="Gender" />
+				<Record field="eyeColor" label="Eye Color" />
+			</ItemDetails>
+		);
 
-    const { getPerson,
-            getStarship,
-            getPersonImage,
-            getStarshipImage } = this.swapiService;
+		const starshipDetails = (
+			<ItemDetails
+				itemId={5}
+				getData={getStarship}
+				getImageUrl={getStarshipImage}
+			>
+				<Record field="model" label="Model" />
+				<Record field="length" label="Length" />
+				<Record field="costInCredits" label="Cost" />
+			</ItemDetails>
+		);
 
-    const personDetails = (
-      <ItemDetails
-        itemId={11}
-        getData={getPerson}
-        getImageUrl={getPersonImage} >
+		return (
+			<ErrorBoundry>
+				<div className="stardb-app">
+					<Header />
 
-        <Record field="gender" label="Gender" />
-        <Record field="eyeColor" label="Eye Color" />
+					<ItemList getData={getAllPeople} onItemSelected={() => {}}>
+						{({ name }) => <span>{name}</span>}
+					</ItemList>
 
-      </ItemDetails>
-    );
-
-    const starshipDetails = (
-      <ItemDetails
-        itemId={5}
-        getData={getStarship}
-        getImageUrl={getStarshipImage}>
-
-        <Record field="model" label="Model" />
-        <Record field="length" label="Length" />
-        <Record field="costInCredits" label="Cost" />
-      </ItemDetails>
-
-    );
-
-    return (
-      <ErrorBoundry>
-        <div className="stardb-app">
-          <Header />
-
-          <Row
-            left={personDetails}
-            right={starshipDetails} />
-        </div>
-      </ErrorBoundry>
-    );
-  }
+					<ItemList getData={getAllPlanets} onItemSelected={() => {}}>
+						{({ name }) => <span>{name}</span>}
+					</ItemList>
+				</div>
+			</ErrorBoundry>
+		);
+	}
 }
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,56 +1,29 @@
 import React, { Component } from 'react';
-
+import { withData } from '../hoc-helper';
 import './item-list.css';
-import SwapiService from "../../services/swapi-service";
-import Spinner from "../spinner/spinner";
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner/spinner';
 
-export default class ItemList extends Component {
+const ItemList = props => {
+	const { data } = props;
 
-  state = {
-    itemList: null
-  };
+	const items = data.map(item => {
+		const { id } = item;
+		const label = props.children(item);
+		return (
+			<li
+				className="list-group-item"
+				key={id}
+				onClick={() => props.onItemSelected(id)}
+			>
+				{label}
+			</li>
+		);
+	});
 
-  componentDidMount() {
+	return <ul className="item-list list-group">{items}</ul>;
+};
 
-    const { getData } = this.props;
+const { getAllPeople } = new SwapiService();
 
-    getData()
-      .then((itemList) => {
-        this.setState({
-          itemList
-        });
-      });
-  }
-
-  renderItems(arr) {
-    return arr.map((item) => {
-      const { id } = item;
-      const label = this.props.children(item);
-
-      return (
-        <li className="list-group-item"
-            key={id}
-            onClick={() => this.props.onItemSelected(id)}>
-          {label}
-        </li>
-      );
-    });
-  }
-
-  render() {
-
-    const { itemList } = this.state;
-
-    if (!itemList) {
-      return <Spinner />;
-    }
-
-    const items = this.renderItems(itemList);
-
-    return (
-      <ul className="item-list list-group">
-        {items}
-      </ul>
-    );
-  }
-}
+export default withData(ItemList, getAllPeople);
