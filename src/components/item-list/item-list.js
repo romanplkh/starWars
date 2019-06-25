d5730ae10eb587cf7f6bd44703f5eef1ee315fad
Roman Pelikh <pelikh.roman@gmail.com>
2019-06-25 14:37:05 -0300

    context

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import { withData } from '../hoc-helper';
+import React from 'react';
+
 import './item-list.css';
-import SwapiService from '../../services/swapi-service';
-import Spinner from '../spinner/spinner';
 
-const ItemList = props => {
-	const { data } = props;
+const ItemList = (props) => {
 
-	const items = data.map(item => {
-		const { id } = item;
-		const label = props.children(item);
-		return (
-			<li
-				className="list-group-item"
-				key={id}
-				onClick={() => props.onItemSelected(id)}
-			>
-				{label}
-			</li>
-		);
-	});
+  const { data, onItemSelected, children: renderLabel } = props;
 
-	return <ul className="item-list list-group">{items}</ul>;
+  const items = data.map((item) => {
+    const { id } = item;
+    const label = renderLabel(item);
+
+    return (
+      <li className="list-group-item"
+          key={id}
+          onClick={() => onItemSelected(id)}>
+        {label}
+      </li>
+    );
+  });
+
+  return (
+    <ul className="item-list list-group">
+      {items}
+    </ul>
+  );
 };
 
-const { getAllPeople } = new SwapiService();
-
-export default withData(ItemList, getAllPeople);
+export default ItemList;
